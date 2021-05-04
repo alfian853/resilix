d5ce7319aee912e7551caba75c999a2fd7aecbd5
alfian <alfian853@gmail.com>
2021-05-05 01:14:48 +0700

    change response type

--- resilience-core/src/test/java/com/kruskal/resilience/core/test/testutil/FunctionalUtil.java
+++ resilience-core/src/test/java/com/kruskal/resilience/core/test/testutil/FunctionalUtil.java
@@ -1,12 +1,18 @@
 package com.kruskal.resilience.core.test.testutil;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 import java.util.function.Consumer;
 
 public class FunctionalUtil {
 
 
-  public static <T> Consumer<T> doNothingConsumer(){
-    return t -> { };
+  public static Consumer<? super Future<?>> doNothingConsumer(){
+    return t -> {
+      try { t.get(); }
+      catch (InterruptedException | ExecutionException ignored) {
+      }
+    };
   }
 
 }
