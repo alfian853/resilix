865da2d423848fd6848ad1b37238e1afa777ef46
alfian <alfian853@gmail.com>
2021-05-09 00:22:04 +0700

    optimize import

--- resilience-core/src/test/java/com/kruskal/resilience/core/test/factory/SlidingWindowFactoryTest.java
+++ resilience-core/src/test/java/com/kruskal/resilience/core/test/factory/SlidingWindowFactoryTest.java
@@ -1,12 +1,10 @@
 package com.kruskal.resilience.core.test.factory;
 
 import com.kruskal.resilience.core.Configuration;
-import com.kruskal.resilience.core.Context;
-import com.kruskal.resilience.core.factory.RetryManagerFactory;
-import com.kruskal.resilience.core.window.SlidingWindowStrategy;
 import com.kruskal.resilience.core.factory.SlidingWindowFactory;
 import com.kruskal.resilience.core.window.CountBasedWindow;
 import com.kruskal.resilience.core.window.SlidingWindow;
+import com.kruskal.resilience.core.window.SlidingWindowStrategy;
 import com.kruskal.resilience.core.window.TimeBasedWindow;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
